b7dd6a282354ffb3cd8eb0d2598fdefe0086943d
frsmtv <fsimitchiev@gmail.com>
2016-12-05 18:11:44 +0100

    dakheur

---
+++ js/main.js
@@ -0,0 +1,26 @@
+jQuery(function($){
+
+  // ==== FADEIN INTRO PAGE ==== //
+  $('body').css('display', 'none');
+  $('body').fadeIn(2000);
+
+  // ==== SCROLLA ==== //
+  $('.animate').scrolla();
+
+  // ==== INIT BIGTEXT PLUGIN ==== //
+  $('#bigtext').bigtext();
+  $('.homebigtext').bigtext();
+
+  // ==== SLIDER ==== //
+
+
+
+
+
+
+
+
+
+
+// ========================= //
+}); // == CLOSING JQUERY == //
